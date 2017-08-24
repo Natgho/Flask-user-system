fef5e9f7fde82902c5e00c8897f12c07359ac543
Natgho <admin@sezerbozkir.com>
2017-08-24 13:52:48 +0300

    project skeleton was created

---
+++ setup.py
@@ -0,0 +1,5 @@
+# Author: Sezer Yavuzer Bozkir <admin@sezerbozkir.com>
+# License: The GNU General Public License v3.0
+# Created Date: 24.08.2017
+# Version: 0.1
+# Website: https://github.com/Natgho/Flask-user-system
