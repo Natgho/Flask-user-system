66f9f7d5eabc1851be63d4d7d8a47282d1831eff
Natgho <admin@sezerbozkir.com>
2017-08-25 11:49:08 +0300

    Base tests added

---
+++ tests/base_tests.py
@@ -0,0 +1,43 @@
+# Author: Sezer Yavuzer Bozkir <admin@sezerbozkir.com>
+# License: The GNU General Public License v3.0
+# Created Date: 24.08.2017
+# Version: 0.1
+# Website: https://github.com/Natgho/Flask-user-system
+
+from setup import app
+import unittest
+
+class FlaskBookshelfTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True
+
+    def tearDown(self):
+        pass
+
+    def test_home_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/')
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
+
+    def test_home_data(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/')
+
+        # assert the response data
+        self.assertEqual(result.data, "Hello World!!!")
